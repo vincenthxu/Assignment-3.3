6384e52ded3b63451ae140724d753fd51c04e237
Vincent Xu <vincent.xu1@outlook.com>
2025-07-23 14:30:00 -0700

    Added method to generate List of mock Students

--- Student.cs
+++ Student.cs
@@ -14,5 +14,14 @@ namespace Assignment_3._3
         public string? Address { get; set; }
         public Month MonthOfAdmission { get; set; }
         public char Grade { get; set; }
+        public static List<Student> MockData(int numberOfEntries = 10)
+        {
+            List<Student> Students = new();
+            for (int i = 0; i < numberOfEntries; i++)
+            {
+                Students.Add(new Student() { Id = i, FirstName = $"Student {i}", LastName = $"{i}", Address = $"Address {i}", MonthOfAdmission = (Month)(i%12), Grade = 'I' });
+            }
+            return Students;
+        }
     }
 }
