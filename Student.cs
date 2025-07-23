15d2aed0381a2038a967c9a7cc4a63839b9b905a
Vincent Xu <vincent.xu1@outlook.com>
2025-07-23 09:11:23 -0700

    Created Student class with properties

---
+++ Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_3._3
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public string? Address { get; set; }
+        public Month MonthOfAdmission { get; set; }
+        public char Grade { get; set; }
+    }
+}
